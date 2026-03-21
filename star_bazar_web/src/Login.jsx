15334fd5fb0165c138e832f3091c512100cd1344
TusharTs9401 <tushar.thakkar@technovate.in>
2026-03-21 20:27:22 +0530

    Update: Login .env

--- star_bazar_web/src/Login.jsx
+++ star_bazar_web/src/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
     setLoading(true)
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/login/', {
+      const response = await fetch(`${import.meta.env.VITE_DJANGO_URL}/login/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
